d5f96e61023c1181549c5801182a2c186f7848a8
Luu45 <contatopyluu@gmail.com>
2023-09-24 12:46:59 -0300

    Atualização de sum para versão 5 v0.2.1

--- CodesC/sum/sum_v5.c
+++ CodesC/sum/sum_v5.c
@@ -1,14 +1,38 @@
 /*
-    v0.0.4 di Luu
+    v0.2.1 di Luu
      5 4 5
 */
 #include <stdio.h>
+#include <stdlib.h>
 
 int sum(int array_nums[], int quant);
 
-int main()
+char main(int argc, char* argv[])
 {
-    int quant, i, res;
+    int quant, i, res, temp_num;
+    int tot = 0;
+
+    if (argc > 1)
+    {
+        // Somando todos os valores passados como argumento.
+        for (i = 1; i < argc; i++)
+        {
+            // Convertendo os valores de argv para int.
+            temp_num = atoi(argv[i]);
+
+            // Verificando se é um valor inteiro válido.
+            if (temp_num == 0 && argv[i][0] != 0)
+            {
+                printf("Voce informou valores inteiros invalidos.");
+                return 1;
+            }
+
+            tot += temp_num;
+        }
+        printf("%d", tot);
+
+        return 0;
+    }
 
     printf("Quantidade de numeros a serem somados: ");
     scanf("%d", &quant);
