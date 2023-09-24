a920d91c9151bdeb1ed34fbfe6bafa5ba5759cd2
Luu45 <contatopyluu@gmail.com>
2023-09-23 21:49:45 -0300

    Atualizando sum.c para versão 2

--- CodesC/sum.c
+++ CodesC/sum.c
@@ -2,7 +2,13 @@
 
 int main()
 {
-    printf("4 + 5 = %d\n", 4 + 5);
+    int num1, num2;
+
+    printf("Digite um valor: ");
+    scanf("%d", &num1);
+    printf("Digite outro valor: ");
+    scanf("%d", &num2);
+    printf("A soma de %d com %d e %d\n", num1, num2, num1 + num2);
     
     return 0;
 }
